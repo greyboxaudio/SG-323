0c52f9c50c1de766cfc0db6c394e7fbefea5358d
Robert <ra.vettel@outlook.com>
2023-12-06 12:37:23 +0100

    Update PluginEditor.cpp
    added flexbox code

--- source/PluginEditor.cpp
+++ source/PluginEditor.cpp
@@ -89,6 +89,8 @@ SG323AudioProcessorEditor::SG323AudioProcessorEditor(SG323AudioProcessor& p)
     // Make sure that before the constructor has finished, you've set the
     // editor's size to whatever you need it to be.
     setSize(475, 350);
+    setResizable(1, 1);
+    setSize(800, 600);
 }
 
 SG323AudioProcessorEditor::~SG323AudioProcessorEditor()
@@ -102,22 +104,75 @@ void SG323AudioProcessorEditor::paint(juce::Graphics& g)
     g.fillAll(getLookAndFeel().findColour(juce::ResizableWindow::backgroundColourId));
 
     g.setColour(juce::Colours::white);
-    g.setFont(15.0f);
-    g.drawFittedText ("v0.6.3", getLocalBounds(), juce::Justification::bottomRight, 1);
+    g.setFont(16.0f);
+    g.drawFittedText ("v0.6.3_scalable", getLocalBounds(), juce::Justification::bottomRight, 1);
 }
 
 void SG323AudioProcessorEditor::resized()
 {
     // This is generally where you'll want to lay out the positions of any
     // subcomponents in your editor..
-    juce::Rectangle<int> bounds = getLocalBounds();
-    noiseButton.setBounds(25,25,100,50);
-    bitReduceButton.setBounds(100,25,100,50);
-    programBox.setBounds(300, 25, 150, 50);
-    inputGainSlider.setBounds(25, 120, 50, 200);
-    highPassSlider.setBounds(100, 120, 50, 200);
-    lowPassSlider.setBounds(175, 120, 50, 200);
-    predelaySlider.setBounds(250, 120, 50, 200);
-    decaySlider.setBounds(325, 120, 50, 200);
-    wetDrySlider.setBounds(400, 120, 50, 200);
+  juce::Rectangle<int> bounds = getLocalBounds();
+  // top flexbox
+  juce::FlexBox flexboxTop;
+  flexboxTop.flexDirection = juce::FlexBox::Direction::row;
+  flexboxTop.flexWrap = juce::FlexBox::Wrap::noWrap;
+  flexboxTop.justifyContent = juce::FlexBox::JustifyContent::flexEnd;
+  flexboxTop.alignContent = juce::FlexBox::AlignContent::center;
+  juce::Array<FlexItem> itemArrayTop;
+  itemArrayTop.add(FlexItem(programBox).withMinWidth(90.0f).withMinHeight(30.0f).withMaxWidth(180.0f).withMaxHeight(60.0f).withMargin(8.0f).withFlex(1));
+  flexboxTop.items = itemArrayTop;
+  flexboxTop.performLayout(bounds.toFloat());
+  // center flexbox
+  juce::FlexBox flexboxLabels;
+  flexboxLabels.flexDirection = juce::FlexBox::Direction::row;
+  flexboxLabels.flexWrap = juce::FlexBox::Wrap::noWrap;
+  flexboxLabels.justifyContent = juce::FlexBox::JustifyContent::flexEnd;
+  flexboxLabels.alignContent = juce::FlexBox::AlignContent::spaceBetween;
+  juce::Array<FlexItem> itemArrayLabels;
+  itemArrayLabels.add(FlexItem(inputGainLabel).withFlex(1));
+  itemArrayLabels.add(FlexItem(highPassLabel).withFlex(1));
+  itemArrayLabels.add(FlexItem(lowPassLabel).withFlex(1));
+  itemArrayLabels.add(FlexItem(predelayLabel).withFlex(1));
+  itemArrayLabels.add(FlexItem(decayLabel).withFlex(1));
+  itemArrayLabels.add(FlexItem(wetDryLabel).withFlex(1));
+  flexboxLabels.items = itemArrayLabels;
+  flexboxLabels.performLayout(bounds.toFloat());
+  // center flexbox
+  juce::FlexBox flexboxCenter;
+  flexboxCenter.flexDirection = juce::FlexBox::Direction::row;
+  flexboxCenter.flexWrap = juce::FlexBox::Wrap::noWrap;
+  flexboxCenter.justifyContent = juce::FlexBox::JustifyContent::spaceBetween;
+  flexboxCenter.alignContent = juce::FlexBox::AlignContent::spaceBetween;
+  juce::Array<FlexItem> itemArrayCenter;
+  itemArrayCenter.add(FlexItem(inputGainSlider).withMinWidth(80.0f).withMinHeight(120.0f).withMargin(8.0f).withFlex(1));
+  itemArrayCenter.add(FlexItem(highPassSlider).withMinWidth(80.0f).withMinHeight(120.0f).withMargin(8.0f).withFlex(1));
+  itemArrayCenter.add(FlexItem(lowPassSlider).withMinWidth(80.0f).withMinHeight(120.0f).withMargin(8.0f).withFlex(1));
+  itemArrayCenter.add(FlexItem(predelaySlider).withMinWidth(80.0f).withMinHeight(120.0f).withMargin(8.0f).withFlex(1));
+  itemArrayCenter.add(FlexItem(decaySlider).withMinWidth(80.0f).withMinHeight(120.0f).withMargin(8.0f).withFlex(1));
+  itemArrayCenter.add(FlexItem(wetDrySlider).withMinWidth(80.0f).withMinHeight(120.0f).withMargin(8.0f).withFlex(1));
+  flexboxCenter.items = itemArrayCenter;
+  flexboxCenter.performLayout(bounds.toFloat());
+  // bottom flexbox
+  juce::FlexBox flexboxBottom;
+  flexboxBottom.flexDirection = juce::FlexBox::Direction::row;
+  flexboxBottom.flexWrap = juce::FlexBox::Wrap::noWrap;
+  flexboxBottom.justifyContent = juce::FlexBox::JustifyContent::flexStart;
+  flexboxBottom.alignContent = juce::FlexBox::AlignContent::center;
+  juce::Array<FlexItem> itemArrayBottom;
+  itemArrayBottom.add(FlexItem(noiseButton).withMinWidth(120.0f).withMinHeight(30.0f).withMargin(8.0f).withFlex(1));
+  itemArrayBottom.add(FlexItem(bitReduceButton).withMinWidth(120.0f).withMinHeight(30.0f).withMargin(8.0f).withFlex(1));
+  //itemArrayBottom.add(FlexItem(versionLabel).withMargin(8.0f).withFlex(1));
+  flexboxBottom.items = itemArrayBottom;
+  flexboxBottom.performLayout(bounds.toFloat());
+  // main flexbox
+  juce::FlexBox flexboxMain;
+  flexboxMain.flexDirection = juce::FlexBox::Direction::column;
+  juce::Array<FlexItem> itemArrayMain;
+  itemArrayMain.add(FlexItem(flexboxTop).withMaxHeight(getHeight() * 0.2f).withFlex(1));
+  itemArrayMain.add(FlexItem(flexboxLabels).withMaxHeight(getHeight() * 0.1f).withFlex(1));
+  itemArrayMain.add(FlexItem(flexboxCenter).withFlex(1));
+  itemArrayMain.add(FlexItem(flexboxBottom).withMaxHeight(getHeight() * 0.1f).withFlex(1));
+  flexboxMain.items = itemArrayMain;
+  flexboxMain.performLayout(getLocalBounds().toFloat());
 }
