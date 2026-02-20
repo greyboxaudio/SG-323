b37dca4f9627fb7d0750fda73e0d35125a11a6bb
Robert <ra.vettel@outlook.com>
2026-02-20 20:19:01 +0100

    arm64x_demo + predelaymod floats
    added separate cmake presets for arm64x demo build to prevent recompilation, changed all predelaymod values explifitely to float to prevent compiler warnings about implicit conversions

--- arm64x_demo.cmake
+++ arm64x_demo.cmake
@@ -1,5 +1,5 @@
 # directory where the link.rsp file generated during arm64 build will be stored
- set(arm64ReproDir "${CMAKE_CURRENT_SOURCE_DIR}/out/repros")
+ set(arm64ReproDir "${CMAKE_CURRENT_SOURCE_DIR}/out/repros_demo")
 
  # This function reads in the content of the rsp file outputted from arm64 build for a target. Then passes the arm64 libs, objs and def file to the linker using /machine:arm64x to combine them with the arm64ec counterparts and create an arm64x binary.
 
@@ -19,7 +19,7 @@
 
  # During the arm64 build, create link.rsp files that containes the absolute path to the inputs passed to the linker (objs, def files, libs).
 
- if("${BUILD_AS_ARM64X}" STREQUAL "ARM64")
+ if("${BUILD_AS_ARM64X_DEMO}" STREQUAL "ARM64")
  	add_custom_target(mkdirs ALL COMMAND cmd /c (if not exist \"${arm64ReproDir}/\" mkdir \"${arm64ReproDir}\" ))
  	foreach (n ${ARM64X_TARGETS})
  		add_dependencies(${n} mkdirs)
@@ -28,7 +28,7 @@
  	endforeach()
 
  # During the ARM64EC build, modify the link step appropriately to produce an arm64x binary
- elseif("${BUILD_AS_ARM64X}" STREQUAL "ARM64EC")
+ elseif("${BUILD_AS_ARM64X_DEMO}" STREQUAL "ARM64EC")
  	foreach (n ${ARM64X_TARGETS})
  		set_arm64_dependencies(${n})
  	endforeach()
